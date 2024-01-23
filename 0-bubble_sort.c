9c14dd682e4cf613bbb1a4884dca21371cb914aa
n1o2h <aythmadnhylt@gmail.com>
2024-01-23 05:43:34 -0800

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * bubble_soert - Function that sorts an array
+ * of integers in ascending order
+ * using the Bubble sort algorithm
+ *
+ * @arrat: array of int
+ * @size: size of array
+ *
+*/
+ void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, j;
+	
+	if (!array || !size)
+		return;
+	i = 0;
+	while (i < size)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+<h1>0x1B. C - Sorting algorithms & Big O</h1>
+<hr>
+<hr>
+<h3>0. Bubble sort</h3>
+<h3>1. Insertion sort</h3>
+<h3>2. Selection sort</h3>
+<h3>3. Quick sort</h3>
+<h3>4. Shell sort - Knuth Sequence</h3>
+<h3>5. Cocktail shaker sort</h3>
+<h3>6. Counting sort</h3>
+<h3>7. Merge sort</h3>
+<h3>8. Heap sort</h3>
+<h3>9. Radix sort</h3>
+<h3>10. Bitonic sort</h3>
+<h3>11. Quick Sort - Hoare Partition scheme</h3>
+<h3>12. Dealer</h3>
+
